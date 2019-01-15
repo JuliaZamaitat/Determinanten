6b4b6a61de00aee443793a224cb9a7d1571674ab
Julia Zamaitat <j.zamaitat@gmail.com>
2019-01-15 13:10:35 +0100

    First commit

---
+++ src/det.java
@@ -0,0 +1,13 @@
+public class det {
+    public static int nrOfMult;
+    
+    //Berechnung mit 1. Normalform
+    public static double calcDet(double[][] A){
+        return Double.NaN; // Durch Ihren Code ersetzen!
+    }
+
+    //Rekursive Berechnung mit Def. L.4.1.1 Skript
+    public static double calcDetRec(double[][] A){
+        return Double.NaN; // Durch Ihren Code ersetzen!
+    }
+}
